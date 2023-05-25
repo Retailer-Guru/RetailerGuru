7bba71e5fc2e1be1b5da26190d5bbbb7103c5fb0
l1sa5 <lisa.alge@outlook.com>
2023-05-25 20:31:12 +0200

    search

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { JwtModule } from '@auth0/angular-jwt';
 import { NgApexchartsModule } from 'ng-apexcharts';
 import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 import { BearerAuthInterceptor } from 'src/clients/bearerInterceptor';
+import { SearchComponent } from './components/search/search.component';
 
 export function tokenGetter(){
   return localStorage.getItem("jwt")
@@ -26,7 +27,8 @@ export function tokenGetter(){
     ProductsviewComponent,
     ProductComponent,
     EditproductComponent,
-    LoginComponent
+    LoginComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule,
